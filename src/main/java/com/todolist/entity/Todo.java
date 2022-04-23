bba53f6f91e3d3f5254c4b86c47566f905c1779a
juhonamnam <namjhnam@gmail.com>
2022-04-23 17:02:42 +0900

    add created timestamp

--- src/main/java/com/todolist/entity/Todo.java
+++ src/main/java/com/todolist/entity/Todo.java
@@ -1,10 +1,14 @@
 package com.todolist.entity;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,4 +32,8 @@ public class Todo {
 
 	@Column(name = "content", length = 100)
 	private String content;
+
+	@Column(name = "created")
+	@CreationTimestamp
+	private Timestamp created;
 }
